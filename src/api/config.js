70a2eeeef5657673c03b60f48ec180500c4c9432
Valeria Maers <valeria.work2022@gmail.com>
2026-07-12 18:19:59 +0300

    fix: move TMDB credentials to environment variables

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,7 @@
-const AUTH_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjNhNWQ4MjcyNjE3ZjZmNzAyMWJhNGM0OTg0NjRmZiIsIm5iZiI6MTY5OTM1ODgxNi4xMjcsInN1YiI6IjY1NGEyODYwNTMyYWNiMDExYjEwYzI5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JaMiiyblV0gutYXA0_BostrvfuMitxlET6G1ohNUjp4';
+const AUTH_TOKEN = import.meta.env.VITE_TMDB_AUTH_TOKEN;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const BASE_URL = `https://api.themoviedb.org/3`;
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/';
-const API_KEY = 'c23a5d8272617f6f7021ba4c498464ff';
 
 export { AUTH_TOKEN, BASE_URL, BASE_IMG_URL, API_KEY };
